35b94ef63fbeb0e45ecd4fae4c7dcea4d47b7b94
Jan Koppe <post@jankoppe.de>
2017-03-01 22:53:36 +0100

    typo in repository url

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ else
 fi
 
 # Let's clone the current state of the aurbot.github.io repository, so we can add & push the package.
-git clone --depth 1 --branch master "https://${GITHUB_TOKEN}${GITHUB_REF:-github.com/aurbot/aurbot.github.io}" gitrepo
+git clone --depth 1 --branch master "https://${GITHUB_TOKEN}@${GITHUB_REF:-github.com/aurbot/aurbot.github.io}" gitrepo
 cd gitrepo
 git config user.name "${GITHUB_USER_NAME:-aurbot}"
 git config user.email "${GITHUB_USER_EMAIL:-aurbot@jankoppe.de}"
