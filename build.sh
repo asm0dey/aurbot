fc1d6a67430ebcc92eca07cfa0b085e474395302
Jan Koppe <post@jankoppe.de>
2017-03-01 14:29:48 +0100

    building things.

--- build.sh
+++ build.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 mkdir -p out
+mkdir -p report
+mkdir -p repo
+
+echo "failed:" > report/failed.yml
+echo "built:" > report/built.yml
 
 for package in $(cat packages); do
-  docker -it --rm -v $(pwd)/out:/home/arch/out jankoppe/arch-aurbuild $package
+  docker run -it --rm -v $(pwd)/out:/home/arch/out jankoppe/arch-aurbuild $package
+  if [ $? -ne 0 ]; then
+    echo "  - $package" >> report/failed.yml
+  else
+    echo "  - $package" >> report/built.yml
+  fi
 done
+
+
+for artefact in $(ls out); do
+  docker run -it --rm -v $(pwd)/out:/out -v $(pwd)/repo:/repo jankoppe/arch repo-add /repo/aurbot.db.tar.gz /out/$artefact
+done
+
+mv out/* repo
+
+rm -r out
