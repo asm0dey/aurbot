998abd9e746b09c0282fb44abee1a4b287616c51
aurbot <aurbot@jankoppe.de>
2017-03-01 22:36:17 +0100

    rewritten build script, now with version check

--- build.sh
+++ build.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
-set -v
-mkdir -p out
-mkdir -p report
-mkdir -p repo
 
-echo "failed:" > report/failed.yml
-echo "built:" > report/built.yml
+# Before we do anything at all, let's first check if we even need to do a full build.
+# This way, we can hopefully save a lot of travis resources.
 
-docker run -it --rm -v $(pwd)/out:/home/arch/out jankoppe/arch-aurbuild $PACKAGE
+LATEST_PKGVER=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${PACKAGE} 2>&1 | grep -Poi '^pkgver=(.*)$')
+LATEST_PKGREL=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${PACKAGE} 2>&1 | grep -Poi '^pkgrel=(.*)$')
 
-if [ $? -ne 0 ]; then
-  echo "  - $PACKAGE" >> report/failed.yml
+BUILT_PKGVER=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgver)
+BUILT_PKGREL=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgrel)
+set -e
+
+if [ "${LATEST_PKGVER}" = "${BUILT_PKGVER}" ] && [ "${LATEST_PKGREL}" = "${BUILT_PKGREL}" ]; then
+  echo ">>> package is already up to date, skip building."
+  exit 0
 else
-  echo "  - $PACKAGE" >> report/built.yml
+  echo ">>> latest package is different from published, start build."
 fi
 
+# Let's clone the current state of the aurbot.github.io repository, so we can add & push the package.
+git config user.name "${GH_USER_NAME:-aurbot}"
+git config user.email "${GH_USER_EMAIL:-aurbot@jankoppe.de}"
+git clone --depth 1 --branch master "https://${GH_REF:-github.com/aurbot/aurbot.github.io}" gitrepo
+cd gitrepo
 
-for artefact in out/*; do
-  docker run -it --rm -v $(pwd)/out:/out -v $(pwd)/repo:/repo jankoppe/arch repo-add /repo/aurbot.db.tar.gz /out/$artefact
-done
+# build package and put into repository directory
+mkdir -p aurbot
+docker run -it --rm -v $(pwd)/aurbot:/home/arch/out jankoppe/arch-aurbuild ${PACKAGE}
 
-mv out/* repo
+# rebuild repository index
+set +e
+rm aurbot/aurbot.{db,files}*
+set -e
 
-rm -r out
+docker run -it --rm -v $(pwd)/aurbot:/out -v $(pwd)/aurbot:/aurbot jankoppe/arch sh -c 'for pkg in /aurbot/*; do repo-add /aurbot/aurbot.db.tar.gz $pkg; done'
+
+# update version metadata
+mkdir -p meta
+
+echo "${LATEST_PKGVER}" > meta/${PACKAGE}.pkgver
+echo "${LATEST_PKGREL}" > meta/${PACKAGE}.pkgrel
+
+# add all files to repo
+git add meta aurbot
+git commit -m "aurbot built ${PACKAGE}-${LATEST_PKGVER}-${LATEST_PKGREL}"
+
+# Only thing left to do now would be to push back to GitHub.
+# Need to look into Git LFS first, though.
+ls -l meta
+ls -l aurbot
