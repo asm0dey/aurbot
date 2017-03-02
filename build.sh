d5d61cf1c1192878f57cf5f6c502a09c96d7af3e
Jan Koppe <post@jankoppe.de>
2017-03-02 18:49:49 +0100

    watch what happens when this baby hits 88mph

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#not really necessary, but be sure.
+if [ "${PACKAGE}" = "__compare__"]; then
+  exit 0
+fi
+
+
 # Let's clone the current state of the aurbot.github.io repository, so we can add & push the package.
 # Set up git lfs before we do anything, so that we don't blow up our repo size. !!! git lfs does not work with github pages, skip for now.
 git clone --depth 1 --branch master "https://${GITHUB_TOKEN}@${GITHUB_REF:-github.com/aurbot/aurbot.github.io}" gitrepo
@@ -11,6 +17,9 @@ git config user.email "${GITHUB_USER_EMAIL:-aurbot@jankoppe.de}"
 #git lfs track 'aurbot.*'
 #git lfs track
 #git add .gitattributes
+
+BUILT_PKGVER=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgver)
+BUILT_PKGREL=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgrel)
 
 # build package and put into repository directory
 mkdir -p aurbot
--- compare_version.sh
+++ compare_version.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
-# Before we do anything at all, let's first check if we even need to do a full build.
-# This way, we can hopefully save a lot of travis resources.
+# Compare lates and built version for each package and generate a new .travis.yml with updated matrix from that.
 
-export LATEST_PKGVER=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${PACKAGE} 2>&1 | grep -Poi '^pkgver=(.*)$' | cut -d= -f2)
-export LATEST_PKGREL=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${PACKAGE} 2>&1 | grep -Poi '^pkgrel=(.*)$' | cut -d= -f2)
+compare () {
+  LATEST_PKGVER=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${1} 2>&1 | grep -Poi '^pkgver=(.*)$' | cut -d= -f2 | sed 's/"//g')
+  LATEST_PKGREL=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${1} 2>&1 | grep -Poi '^pkgrel=(.*)$' | cut -d= -f2 | sed 's/"//g')
 
-export BUILT_PKGVER=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgver)
-export BUILT_PKGREL=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgrel)
-set -e
+  BUILT_PKGVER=$(curl -s --fail https://aurbot.github.io/meta/${1}.pkgver)
+  BUILT_PKGREL=$(curl -s --fail https://aurbot.github.io/meta/${1}.pkgrel)
 
-if [ "${LATEST_PKGVER}" = "${BUILT_PKGVER}" ] && [ "${LATEST_PKGREL}" = "${BUILT_PKGREL}" ]; then
-  echo ">>> package is already up to date, skip building."
+  if [ "${LATEST_PKGVER}" = "${BUILT_PKGVER}" ] && [ "${LATEST_PKGREL}" = "${BUILT_PKGREL}" ]; then
+    return true
+  else
+    false
+  fi
+}
+
+if [ "${PACKAGE}" = "__compare__"]; then
+  # start with empty matrix list
+  cp .travis.yml.empty .travis.yml
+
+  # add packages to matrix that are not up-to-date
+  while read pkg do;
+    if compare $pkg; then
+      echo "    - PACKAGE=$pkg" >> .travis.yml
+  done <packages
+
+  # always add compare check to end of list
+  echo "    - PACKAGE=__compare__" >> .travis.yml
+  
+  # commit new .travis.yml to repo to trigger new builds.
+  git config user.name "${GITHUB_USER_NAME:-aurbot}"
+  git config user.email "${GITHUB_USER_EMAIL:-aurbot@jankoppe.de}"
+  git add .travis.yml
+  git commit -m "aurbot added out-of-date packages to .travis.yml"
+  git push
   exit 0
-else
-  echo ">>> latest package is different from published, start build."
 fi
