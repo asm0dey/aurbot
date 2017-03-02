b78fe43cd151771bb15ae392bce7b58942585910
Jan Koppe <post@jankoppe.de>
2017-03-02 09:52:55 +0100

    compare versions before doing anything.

--- build.sh
+++ build.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Before we do anything at all, let's first check if we even need to do a full build.
-# This way, we can hopefully save a lot of travis resources.
-
-LATEST_PKGVER=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${PACKAGE} 2>&1 | grep -Poi '^pkgver=(.*)$')
-LATEST_PKGREL=$(curl -s https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=${PACKAGE} 2>&1 | grep -Poi '^pkgrel=(.*)$')
-
-BUILT_PKGVER=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgver)
-BUILT_PKGREL=$(curl -s --fail https://aurbot.github.io/meta/${PACKAGE}.pkgrel)
-set -e
-
-if [ "${LATEST_PKGVER}" = "${BUILT_PKGVER}" ] && [ "${LATEST_PKGREL}" = "${BUILT_PKGREL}" ]; then
-  echo ">>> package is already up to date, skip building."
-  exit 0
-else
-  echo ">>> latest package is different from published, start build."
-fi
-
 # Let's clone the current state of the aurbot.github.io repository, so we can add & push the package.
 # Set up git lfs before we do anything, so that we don't blow up our repo size.
 git clone --depth 1 --branch master "https://${GITHUB_TOKEN}@${GITHUB_REF:-github.com/aurbot/aurbot.github.io}" gitrepo
